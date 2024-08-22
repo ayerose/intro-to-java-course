5ba3fa39af5b0dd2503bf5c434c2f46141b9860f
ayerose <reya-k@outlook.de>
2024-08-22 18:09:07 +0100

    uodated fizzbuzz file

--- exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
@@ -11,5 +11,14 @@ public class FizzBuzz {
         return "Fizz";
     } else return Integer.toString(number);
 
-   }    
+   }   
+   public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        
+     
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(fizzBuzz.get(i));
+        }
+    }
+ 
 }
