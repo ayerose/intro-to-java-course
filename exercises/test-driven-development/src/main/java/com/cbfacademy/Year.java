8de1785691c7129006269f43cb4ea353b89a1829
ayerose <reya-k@outlook.de>
2024-08-24 15:37:19 +0100

    added leap year tests

--- exercises/test-driven-development/src/main/java/com/cbfacademy/Year.java
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/Year.java
@@ -1,29 +1,21 @@
 package com.cbfacademy;
 public class Year {
 
-    public boolean isLeap(int year) {
-        if (year % 400 == 0) {
+    private final Integer year;
+    public Year(Integer year) {
+        this.year = year;
+
+    }
+    public boolean isLeap() {
+        if (this.year % 400 == 0) {
             return true;
-        } else if (year % 100 == 0) {
+        } else if (this.year % 100 == 0) {
             return false;
-        } else if (year % 4 == 0) {
+        } else if (this.year % 4 == 0) {
             return false;
         } else {
             return false;
         }
     }
-       public static void main(String[] args) {
-        Year year = new Year();
 
-
-        int testYear1 = 2020;
-        int testYear2 = 1900;
-        int testYear3 = 2000;
-        int testYear4 = 2023;
-
-        System.out.println(testYear1 + " is a leap year: " + year.isLeap(testYear1));  
-        System.out.println(testYear2 + " is a leap year: " + year.isLeap(testYear2));  
-        System.out.println(testYear3 + " is a leap year: " + year.isLeap(testYear3));  
-        System.out.println(testYear4 + " is a leap year: " + year.isLeap(testYear4));  
-    }
 }
--- exercises/test-driven-development/src/test/java/com/cbfacademy/AppTest.java
+++ exercises/test-driven-development/src/test/java/com/cbfacademy/AppTest.java
@@ -1,9 +1,19 @@
 package com.cbfacademy;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.Year;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -17,4 +27,37 @@ public class AppTest {
 
         assertThat(app, is(notNullValue()));
     }
-}
+
+   /*  @Test
+    @DisplayName("1700 is not a leap year")
+    public void seventeenHunIsNotALeapYear() {
+        assertEquals(false, Year.isLeap(1700));
+    }
+
+    @Test
+    @DisplayName("2000 is a leap year")
+    public void twoThousandIsALeapYear(){
+        assertEquals(true, Year.isLeap(2000));
+    }
+
+    @Test
+    @DisplayName("2030 is not a leap year")
+    public void twoThirtyiSNOTaLeapYear(){
+        assertEquals(false, Year.isLeap(2030));
+} */
+ 
+    static Stream <Arguments> inputAndOutputProvider(){
+        return Stream.of(
+            Arguments.of(1600, true),
+            Arguments.of(1700, false), 
+            Arguments.of(2000, true),
+            Arguments.of(2030, false)
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("inputAndOutputProvider")
+    @DisplayName("Determine whether leap year or not")
+    public void returnExpectedMethod(int year, boolean expected) {
+        assertEquals(expected, Year.isLeap(year));
+    }
+    } 
